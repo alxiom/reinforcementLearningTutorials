494de124db51d9ecddee25e33ca95e8bdaa08abe
alexkim <hyoungseok.k@gmail.com>
2019-02-07 15:33:17 +0900

    [Python] refactor cart pole example

--- Lab7-02-DQN2013CartPolePT.py
+++ Lab7-02-DQN2013CartPolePT.py
@@ -6,10 +6,10 @@ from torch import nn
 from collections import deque
 
 seed = 42
-
-env = gym.make('CartPole-v0').unwrapped
+step_limit = 5000
+env = gym.make('CartPole-v0')
+env._max_episode_steps = step_limit
 env.seed(seed)
-
 torch.manual_seed(seed)
 
 input_size = env.observation_space.shape[0]
@@ -23,11 +23,11 @@ learning_rate = 1e-1
 
 gamma = 0.99
 max_episodes = 5000
-half_eps_episode = max_episodes * 0.02
+initial_epsilon = 1.0
+epsilon_decay = 0.99
 
-break_test_count = 4
-break_test_avg = 5000
-step_limit = 5000
+break_episodes = 4
+break_reward = 4500
 
 
 class DQN(nn.Module):
@@ -89,21 +89,20 @@ def main():
     dqn = DQN(input_size, hidden_size, output_size)
     dqn.train()
 
-    optimizer = torch.optim.Adam(dqn.parameters(), lr=learning_rate)
     criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(dqn.parameters(), lr=learning_rate)
 
     replay_buffer = deque(maxlen=replay_memory)
-    step_counts = deque(maxlen=break_test_count)
+    reward_history = deque(maxlen=break_episodes)
 
     for episode in range(max_episodes):
         state = env.reset()
-        e = 1.0 / ((episode / half_eps_episode) + 1.0)
-        rewards = 0
-        step_count = 0
+        eps = initial_epsilon * np.power(epsilon_decay, episode)
+        episode_reward = 0
         done = False
 
         while not done:
-            if np.random.rand(1) < e:
+            if np.random.rand(1) < eps:
                 action = env.action_space.sample()
             else:
                 x = torch.Tensor(state).float()
@@ -112,23 +111,22 @@ def main():
 
             next_state, reward, done, _ = env.step(action)
             if done:
-                reward = -100.0
+                reward = -100
 
             replay_buffer.append((state, action, reward, next_state, done))
 
-            rewards += reward
-            step_count += 1
+            episode_reward += reward
             state = next_state
 
-            if step_count > step_limit:  # good enough
+            if episode_reward >= break_reward:  # good enough
                 break
 
-        print(f"episode = {episode + 1}, rewards = {rewards}, steps = {step_count}")
+        print(f"episode = {episode + 1}, rewards = {episode_reward}")
 
-        step_counts.append(step_count)
-        step_avg = np.mean(step_counts)
-        if step_avg > break_test_avg:
-            print(f"game cleared in {episode} episodes with avg step {step_avg}")
+        reward_history.append(episode_reward)
+        reward_avg = np.mean(reward_history)
+        if reward_avg == break_reward:
+            print(f"game cleared in {episode} episodes with avg step {reward_avg}")
             break
 
         if ((episode + 1) % training_interval == 0) & (len(replay_buffer) > batch_size * training_interval):
